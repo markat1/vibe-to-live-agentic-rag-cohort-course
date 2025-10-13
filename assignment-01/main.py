0934f3e1bd03aef1effff1db91e8116f170063b1
markat1 <markt@outlook.dk>
2025-10-13 17:13:32 +0000

    changed from arize-phoenix to arize-phoenix-otel

--- assignment-01/main.py
+++ assignment-01/main.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI
 from agents import Runner
 from rag_agent import create_rag_agent
 import os
-from phoenix.trace import using_project
 from phoenix.otel import register
 
 app = FastAPI()
 
-tracer_provider = register(
+register(
         project_name=os.getenv("PHOENIX_PROJECT_NAME"),
         endpoint=os.getenv("PHOENIX_COLLECTOR_ENDPOINT"),
         protocol="http/protobuf",
@@ -15,9 +14,8 @@ tracer_provider = register(
         batch=True,
 )
 
-
 @app.get("/chat")
 async def ask_agent(question: str):
-    with using_project(os.getenv("PHOENIX_PROJECT_NAME")):
-        result = await Runner.run(create_rag_agent(), question)
-        return {"answer": result.final_output}
+    result = await Runner.run(create_rag_agent(), question)
+    return {"answer": result.final_output}
+        
