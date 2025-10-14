7a079e968e3a44c35cdd4c6f314e20ecf19dcd90
markat1 <markt@outlook.dk>
2025-10-14 15:23:45 +0000

    removed unnessary and aded openaiinstrumentor again

--- assignment-01/main.py
+++ assignment-01/main.py
@@ -3,16 +3,17 @@ from agents import Runner
 from rag_agent import create_rag_agent
 import os
 from phoenix.otel import register
+from openinference.instrumentation.openai import OpenAIInstrumentor
+
 
 app = FastAPI()
 
-register(
+tracer_provider = register(
         project_name=os.getenv("PHOENIX_PROJECT_NAME"),
         endpoint=os.getenv("PHOENIX_COLLECTOR_ENDPOINT"),
-        protocol="http/protobuf",
-        auto_instrument=True,
-        batch=True,
 )
+
+OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
 
 @app.get("/chat")
 async def ask_agent(question: str):
