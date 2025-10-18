7cd848c225fad47602ab4fd9df4edc9c3dfe2140
markat1 <markt@outlook.dk>
2025-10-18 13:23:51 +0000

    added fastapi + server

--- assignment-002/mcp/server.py
+++ assignment-002/mcp/server.py
@@ -1,3 +1,20 @@
 from fastmcp import FastMCP
 
-mcp = FastMCP("Demo Server 🚀")
+mcp = FastMCP("weather-server")
+mcp.run(transport="streamable-http", host="0.0.0.0", port=8000)
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@mcp.resource("config://version")
+def get_version():
+    return "2.0.1"
+
+@mcp.resource("users://{user_id}/profile")
+def get_profile(user_id: int):
+    return {"name": f"User {user_id}", "status": "active"}
+
+if __name__ == "__main__":
+    mcp.run() # Runs the server in STDIO mode by default
